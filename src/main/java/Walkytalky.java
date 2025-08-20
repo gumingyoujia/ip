15c19eede982de2c503ee0b885fc006dd6beccac
GU MINGYOUJIA <e1454260@u.nus.edu>
2025-08-20 22:03:27 +0800

    added welcome message

--- src/main/java/Walkytalky.java
+++ src/main/java/Walkytalky.java
@@ -1,10 +1,20 @@
 public class Walkytalky {
+    public static void printWelcomeMessage() {
+        System.out.println("—".repeat(60));
+        String logo = "▗▖ ▗▖ ▗▄▖ ▗▖   ▗▖ ▗▖▗▖  ▗▖▗▄▄▄▖▗▄▖ ▗▖   ▗▖ ▗▖▗▖  ▗▖\n" +
+                "▐▌ ▐▌▐▌ ▐▌▐▌   ▐▌▗▞▘ ▝▚▞▘   █ ▐▌ ▐▌▐▌   ▐▌▗▞▘ ▝▚▞▘ \n" +
+                "▐▌ ▐▌▐▛▀▜▌▐▌   ▐▛▚▖   ▐▌    █ ▐▛▀▜▌▐▌   ▐▛▚▖   ▐▌  \n" +
+                "▐▙█▟▌▐▌ ▐▌▐▙▄▄▖▐▌ ▐▌  ▐▌    █ ▐▌ ▐▌▐▙▄▄▖▐▌ ▐▌  ▐▌  \n" +
+                "                                                   ";
+        System.out.println(" Hi!!! I am your\n" + logo);
+        System.out.println(" You can ask me anything and I will always be here:)");
+        System.out.println("—".repeat(60));
+        System.out.println(" What can I do for you today?");
+        System.out.println("—".repeat(60));
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("—".repeat(60));
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printWelcomeMessage();
     }
 }
