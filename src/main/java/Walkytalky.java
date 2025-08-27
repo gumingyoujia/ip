f6bece4067ff64395373b66a81025cb3a282818e
GU MINGYOUJIA <E1454260@u.nus.edu>
2025-08-27 10:12:06 +0800

    Add echo method

--- src/main/java/Walkytalky.java
+++ src/main/java/Walkytalky.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Walkytalky {
     public static void printWelcomeMessage() {
         System.out.println("—".repeat(60));
@@ -11,10 +12,21 @@ public class Walkytalky {
         System.out.println("—".repeat(60));
         System.out.println(" What can I do for you today?");
         System.out.println("—".repeat(60));
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("—".repeat(60));
     }
+    public static void echo(Scanner in){
+        while (true){
+            String input = in.nextLine();
+            if (input.equalsIgnoreCase("bye")){
+                System.out.println("—".repeat(60)+"\nBye. Hope to see you again soon!\n"+"—".repeat(60));
+                return;
+            }
+            System.out.println("—".repeat(60)+"\n"+input+"\n"+"—".repeat(60));
+        }
+    }
+
     public static void main(String[] args) {
         printWelcomeMessage();
+        Scanner in = new Scanner(System.in);
+        echo(in);
     }
 }
